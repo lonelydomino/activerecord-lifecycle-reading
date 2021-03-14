60d9972e5195f0fd3a548ccdb71f81914828fecd
Michael Martinez <zeroeclipse@live.com>
2021-03-14 11:54:17 -0700

    commit tree 3f9b00c25176dd3349e648be2583e935e6e1f209

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,21 @@
+
+
 class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+
+#  Here is a rule of thumb: **Whenever you are modifying an attribute of the
+#  model, use `before_validation`. If you are doing some other action, then use
+# `before_save`.**
+
+  before_validation :make_title_case
+
+# before_save :make_title_case
+before_save :email_author_about_post
+#   We use `before_save` for actions that need to occur that aren't
+# modifying the model
+# itself.
 
   private
 
@@ -14,4 +28,12 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
 end
+# Before you move on, let's cover one last callback that is useful:
+# `before_create`. `before_create` is very close to `before_save` with one major
+# difference: it only gets called when a model is created for the first time.
+# This means not every time the object is persisted, just when it is **new**.
